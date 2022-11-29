e9cd6ea9492fa4947a6162697013c2afcae23d06
KashElcock <116089696+KashElcock@users.noreply.github.com>
2022-11-29 14:30:41 -0500

    complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
-
+    public static void main(String[] args ) {
+        Scanner scanner = null;
+        String userInput;
+        boolean loggedIn = false;
+        while (!loggedIn) {
+            int i = 1;
+            System.out.println("Please enter your name");
+            scanner = new Scanner(System.in);
+            userInput = String.valueOf(scanner.nextLine());
+            if (userInput.equalsIgnoreCase("Alice") || userInput.equalsIgnoreCase("Bob")){
+                System.out.println("Welcome " + userInput + "!");
+                loggedIn = true;
+            }
+            else {
+                System.out.println("Unknown user!");
+            }
+        }
     }
 }
